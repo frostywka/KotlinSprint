2e021760fce2a55ffc412ec7d437394fba10fe15
Ilya Ryzhov <isryjov@mts.ru>
2025-03-05 20:50:09 +0300

    KS-1-1 : refactoring homework 5 for lesson1 (variable)

--- src/main/kotlin/lesson_2_variables/lesson1_1.kt
+++ src/main/kotlin/lesson_2_variables/lesson1_1.kt
@@ -1,7 +1,7 @@
 package org.example.lesson_2_variables
 
 fun main() {
-    var numberOfOrders: Int = 75 // var потому что значение всяко будет меняться
+    var numberOfOrders: Int = 75
     val thanksForPurchase: String = "Спасибо за заказ. Приходите еще!"
 
     println("Было обработано $numberOfOrders заказов. Каждый заказ сопроводили текстом: \"$thanksForPurchase\" ")
