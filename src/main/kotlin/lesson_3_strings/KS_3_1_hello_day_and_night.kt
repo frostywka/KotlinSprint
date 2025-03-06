23fd14d77a522f0d85beacb4e79205e26a62def3
Ilya Ryzhov <isryjov@mts.ru>
2025-03-06 17:41:04 +0300

    KS-3-1 : add homework 3 for lesson_2 (Strings)

---
+++ src/main/kotlin/lesson_3_strings/KS_3_1_hello_day_and_night.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3_strings
+
+fun main() {
+
+    val name = "Илья"
+    var greetings = "Добрый день"
+
+    println("$greetings, $name!")
+
+    greetings = "Добрый вечер"
+    println("$greetings, $name!")
+
+}
