8b61883420729cc49ae866510deee5367431abc7
Ilya Ryzhov <isryjov@mts.ru>
2025-03-06 18:00:28 +0300

    KS-3-3 : add homework 3 for lesson_3 (Strings)

---
+++ src/main/kotlin/lesson_3_strings/KS_3_3_multiplication_table.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3_strings
+
+fun main() {
+    val number = 2
+
+    println("""
+        $number * 1 = ${number*1}
+        $number * 2 = ${number*1}
+        $number * 3 = ${number*1}
+        $number * 4 = ${number*1}
+        $number * 5 = ${number*1}
+        $number * 6 = ${number*1}
+        $number * 7 = ${number*1}
+        $number * 8 = ${number*1}
+        $number * 9 = ${number*1}
+        $number * 10 = ${number*1}
+    """.trimIndent())
+}
