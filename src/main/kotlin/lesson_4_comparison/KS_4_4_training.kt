855f14fea609481389dff1732169782b3c3d0144
Ilya Ryzhov <isryjov@mts.ru>
2025-03-07 22:24:59 +0300

    KS-4-4 : add homework 4 for lesson_4 (Strings)

--- src/main/kotlin/lesson_4_comparison/KS_4_4_training.kt
+++ src/main/kotlin/lesson_4_comparison/KS_4_4_training.kt
@@ -1,32 +1,22 @@
 package org.example.lesson_4_comparison
 
-/*
-Программа тренировок разбивает упражнения на две группы: упражнения для мышц рук/пресса и ног/спины.
-Она рекомендует делать упражнения на каждую группу мышц через день. Пользователь начал занятия с рук и пресса.
- Напиши программу, которая будет сообщать, какие упражнения нужно выполнить сегодня.
-
-Предположим, что сегодня 5 день тренировок. В первый день выполнялись упражнения для рук и пресса.
-Выведи сообщение в виде нескольких красивых ровных строк.
-
-Вывод должен быть таким:
-
-Упражнения для рук:       true
-Упражнения для ног:       false
-Упражнения для спины:  false
-Упражнения для пресса: true
-
-- все сообщение должно печататься в одном println();
-- при изменении значения переменной дня тренировки на 1 день вперед, вывод должен меняться.
- */
+const val isTrainingForHandsAndPress: Boolean = true
+const val isTrainingForLegsAndBack: Boolean = true
 
 fun main() {
 
-    isTrainingForHeads
-    isTrainingForLegs
-    isTrainingForBack
-    isTrainingForPress
+    var trainingDay = 5
 
-    println("""
+    val evenTrainingDay = trainingDay == 5 && isTrainingForHandsAndPress == true
+    val oddTrainingDay = trainingDay == 6 && isTrainingForLegsAndBack == true
+
+    println(
+        """
+        Упражнения для рук:    $evenTrainingDay
+        Упражнения для ног:    $oddTrainingDay
+        Упражнения для спины:  $oddTrainingDay
+        Упражнения для пресса: $evenTrainingDay
         
-    """.trimIndent())
+    """.trimIndent()
+    )
 }
