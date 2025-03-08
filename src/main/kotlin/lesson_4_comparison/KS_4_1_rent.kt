ba5e24f13de25593f64cad4e8909eaac1dc3f289
Ilya Ryzhov <isryjov@mts.ru>
2025-03-08 12:36:05 +0300

    KS-4-1 : update homework 1 for lesson_4 (comparison)

--- src/main/kotlin/lesson_4_comparison/KS_4_1_rent.kt
+++ src/main/kotlin/lesson_4_comparison/KS_4_1_rent.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_4_comparison
 
+const val TOTAL_TABLES = 13
+
 fun main() {
 
-    val totalTables = 13
     val tablesToday = 13
     val tablesTomorrow = 9
 
-    println("Доступность столик на сегодня : ${totalTables > tablesToday} ")
-    println("Доступность столик на завтра : ${totalTables > tablesTomorrow} ")
+    println("Доступность столик на сегодня : ${TOTAL_TABLES > tablesToday} ")
+    println("Доступность столик на завтра : ${TOTAL_TABLES > tablesTomorrow} ")
 
 }
