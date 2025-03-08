bc91056b5f88d3b714c41732688b14040470c72b
Ilya Ryzhov <isryjov@mts.ru>
2025-03-08 09:04:30 +0300

    KS-2-3 : update 3 for lesson_2 (Strings)

--- src/main/kotlin/lesson_2_arithmetic/KS-2-3-trainTime.kt
+++ src/main/kotlin/lesson_2_arithmetic/KS-2-3-trainTime.kt
@@ -1,7 +1,7 @@
 package org.example.lesson_2_arithmetic
 
-const val minInHour = 60
-const val hourInDay = 24
+const val MIN_IN_HOUR = 60
+const val HOUR_IN_DAY = 24
 
 fun main() {
 
@@ -9,9 +9,9 @@ fun main() {
     val startTimeMinutes = 39
     val travelTime = 457
 
-    val totalMinutes = startTimeHours * minInHour + travelTime + startTimeMinutes
-    val arrivalTimeHours = (totalMinutes / minInHour) % hourInDay
-    val arrivalTimeMinutes = totalMinutes % minInHour
+    val totalMinutes = startTimeHours * MIN_IN_HOUR + travelTime + startTimeMinutes
+    val arrivalTimeHours = (totalMinutes / MIN_IN_HOUR) % HOUR_IN_DAY
+    val arrivalTimeMinutes = totalMinutes % MIN_IN_HOUR
 
     val formatStartTime = String.format("%02d:%02d", startTimeHours, startTimeMinutes)
     val formatArrivalTime = String.format("%02d:%02d", arrivalTimeHours, arrivalTimeMinutes)
