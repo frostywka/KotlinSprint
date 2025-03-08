9051547a560006ac8e7dffcc31c55d292e997118
Ilya Ryzhov <isryjov@mts.ru>
2025-03-08 09:08:00 +0300

    KS-2-4 : update 4 for lesson_2 (Strings)

--- src/main/kotlin/lesson_2_arithmetic/KS_2_4_buff.kt
+++ src/main/kotlin/lesson_2_arithmetic/KS_2_4_buff.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_2_arithmetic
 
-const val denominatorForPercent = 100f
+const val DENOMINATOR_FOR_PERCENT = 100f
 
 fun main() {
     val krystal = 7
     val iron = 11
     val percent = 20
-    val buff = percent / denominatorForPercent
+    val buff = percent / DENOMINATOR_FOR_PERCENT
 
     val krystalUnderBuff: Int = ((krystal * buff).toInt())
     val ironUnderBuff: Int = (iron * buff).toInt()
