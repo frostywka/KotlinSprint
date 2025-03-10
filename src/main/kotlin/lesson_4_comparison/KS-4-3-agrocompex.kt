c71c817d8dab6c28e6f485ef51055736ac485d0a
Ilya Ryzhov <isryjov@mts.ru>
2025-03-10 10:22:20 +0300

    KS-4-3 : update homework 3 (agrocompex/погода в агрокомплексе категории  for lesson_4 (comparison/сравнение)

--- src/main/kotlin/lesson_4_comparison/KS-4-3-agrocompex.kt
+++ src/main/kotlin/lesson_4_comparison/KS-4-3-agrocompex.kt
@@ -3,11 +3,21 @@ package org.example.lesson_4_comparison
 const val IS_WEATHER_SUNNY: Boolean = true
 const val IS_TENT_OPEN = true
 const val HIMIDITY = 20
-const val SEASON = "Не Зима"
+const val SEASON = "Зима"
 
 fun main() {
 
-    val check = IS_WEATHER_SUNNY === true && IS_TENT_OPEN === true && HIMIDITY === 20 && SEASON === "Зима"
+    val isWeatherSunnyToday = true
+    val isTentOpenToday = true
+    val himidityToday = 20
+    val seasonToday = "Весна"
+
+    val checkSun = IS_WEATHER_SUNNY == isWeatherSunnyToday
+    val checkTent = IS_TENT_OPEN == isTentOpenToday
+    val checkHimidity = HIMIDITY == himidityToday
+    val checkSeason = SEASON == seasonToday
+
+    val check = checkSun && checkTent && checkHimidity && checkSeason
 
     println("Благоприятные ли условия сейчас для роста бобовых? : $check ")
 }
