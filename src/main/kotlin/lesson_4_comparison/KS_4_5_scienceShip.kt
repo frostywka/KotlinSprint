3c9508eded5d9203466dcb6381dc91e3aa27004d
Ilya Ryzhov <isryjov@mts.ru>
2025-03-12 12:22:36 +0300

    KS-4-5: update homework 5(Экспедиция научного судна) for lesson 4

--- src/main/kotlin/lesson_4_comparison/KS_4_5_scienceShip.kt
+++ src/main/kotlin/lesson_4_comparison/KS_4_5_scienceShip.kt
@@ -2,12 +2,13 @@ package org.example.lesson_4_comparison
 
 const val MIN_CREW = 55
 const val MAX_CREW = 75
+const val RECOMMENDATION_CREW = 70
 const val BOX = 50
 
 fun main() {
 
-    println("корабль имеет повреждений? true/false ")
-    val isDamageInput = readln().toBoolean()
+    println("корабль имеет повреждений? (true/false) ")
+    val isDamage = readln().toBoolean()
 
     println("Количество экипажа: Int ")
     val crewInput = readln().toInt()
@@ -15,16 +16,19 @@ fun main() {
     println("Количество ящиков на борту: Int ")
     val boxInput = readln().toInt()
 
-    println("Погода: ")
-    val isWeatherInput = readln().toBoolean()
+    println("Погода Ясная: (true/false)")
+    val isWeatherFine = readln().toBoolean()
 
-    val isDamage = isDamageInput == true || isDamageInput == false
     val isCrew = crewInput in MIN_CREW..MAX_CREW
     val isBox = BOX < boxInput
-    val isWeather = true == isWeatherInput || false == isWeatherInput
+    val isRecommendationCrew = RECOMMENDATION_CREW == crewInput
 
-    val check = isDamage && isCrew && isBox && isWeather
+    val checkOne = !isDamage && isCrew && isBox
+    val checkTwo = isRecommendationCrew && isWeatherFine && isBox
 
-    println("Кораблю разрешено выплыть: $check")
+    val totalCheck = checkOne || checkTwo
+    println("$checkOne $checkTwo")
+
+    println("Кораблю разрешено выплыть: $totalCheck")
 
 }
