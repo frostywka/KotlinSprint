cd3a8fb64269a7b5c38bf73943d8586c3848b14d
Ilya Ryzhov <isryjov@mts.ru>
2025-03-20 11:23:25 +0300

    KS-7-3 : update homework 2 (Вывести четные числа) для урока 7 ( цикл FOR )

--- src/main/kotlin/lesson_7_for/KS_7_3_even_numbers.kt
+++ src/main/kotlin/lesson_7_for/KS_7_3_even_numbers.kt
@@ -4,10 +4,8 @@ fun main() {
     println("ввести число:")
     val number = readln().toInt()
 
-    for (i in 0..number) {
-        if (i == 0) {
-            continue
-        } else if (i % 2 == 0) {
+    for (i in 1..number) {
+        if (i % 2 == 0) {
             println(i)
         }
     }
