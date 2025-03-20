0f9995a320057d4820a305145421535b7ee53f83
Ilya Ryzhov <isryjov@mts.ru>
2025-03-20 11:59:16 +0300

    KS-7-4 : add homework 4 (таймер через for) для урока 7 ( цикл FOR )

---
+++ src/main/kotlin/lesson_7_for/KS_7_4_timer.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_7_for
+
+fun main() {
+    println("Введите кол-во секунд для отчета:")
+    val second = readln().toInt()
+
+    for (i in second downTo 0) {
+        if (i == 0) {
+            println("Время вышло")
+        } else {
+            println("Осталось до конца $i секунд")
+        }
+    }
+
+}
