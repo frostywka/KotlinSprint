754fe82985f54cd52098e004d014f14208e4bd2b
Ilya Ryzhov <isryjov@mts.ru>
2025-03-21 10:49:43 +0300

    KS-7-5 update homework 5 (Генерация нового пароля) урок 7 (FOR)

--- src/main/kotlin/lesson_7_for/KS_7_5_newGeneratePassword.kt
+++ src/main/kotlin/lesson_7_for/KS_7_5_newGeneratePassword.kt
@@ -1,31 +1,22 @@
 package org.example.lesson_7_for
 
-/*
-Создай программу, которая генерирует более сложные пароли.
-
-– пароль должен содержать цифры, строчные и заглавные буквы - должны присутствовать все три вида символов;
-– цифры и буквы должны располагаться в случайном порядке;
-– длина пароля должна задаваться пользователем;
-– минимальная длина пароля 6 символов.
- */
-
 fun main() {
     println("Введите длину желаемого пароля, минимальная длина 6 символов: ")
     val length = readln().toInt()
     val numbers = 0..9
     val lower = 'a'..'z'
     val uppper = 'A'..'Z'
+
     var newPassword = ""
 
 // решение 1
-    val password = (0..9) + ('a'..'z') + ('A'..'Z')
-    val shuffle = password.shuffled()
-    val size = shuffle.joinToString("", limit = length)
+    val password = numbers + lower + uppper
+    val size = password.shuffled().take(length).joinToString("")
     println("Пароль $size через решение номер 1")
 
 // решение 2
     for ( i in length downTo 1){
-        newPassword += (numbers+lower+uppper).random()
+        newPassword += (password).random()
     }
     println("Пароль $newPassword через решение номер 2")
 }
