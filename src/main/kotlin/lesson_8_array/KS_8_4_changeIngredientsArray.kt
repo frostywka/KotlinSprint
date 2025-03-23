65ea12df3e97f832d6bd2a66f817c2eab8e16915
Ilya Ryzhov <isryjov@mts.ru>
2025-03-23 15:15:16 +0300

    KS-8-4 add homework 4 ( Изменение в массивае данных) урок 8 (arrayOf/массивы)

--- src/main/kotlin/lesson_8_array/KS_8_4_changeIngredientsArray.kt
+++ src/main/kotlin/lesson_8_array/KS_8_4_changeIngredientsArray.kt
@@ -14,4 +14,28 @@ package org.example.lesson_8_array
 
 fun main() {
 
+    println("Ингредиенты в текущем списке:")
+    val listOfIngredients = arrayOf("яйцо", "майонез", "картошка", "морковь", "сахар", "колбаса")
+    for (i in listOfIngredients) {
+        println(i)
+    }
+
+    println("Какой ингредиент Вы хотите заменить?")
+    val oldIngredient = readln()
+    println("Какой ингредиент Вы хотите Добавить?")
+    val newIngredient = readln()
+
+    for (i in listOfIngredients) {
+        if (i == oldIngredient) {
+            println("Ингредиент есть")
+            val indexIngredients = listOfIngredients.indexOf(oldIngredient)
+            listOfIngredients[indexIngredients] = newIngredient
+
+            for (j in listOfIngredients) {
+                println("Готово! Вы сохранили следующий список: [$j]")
+            }
+            return
+        }
+    }
+    println("Ингредиента $oldIngredient в рецепте нет")
 }
