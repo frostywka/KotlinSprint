c485884200c4ea1ad01661cb9462104886082d1a
Ilya Ryzhov <isryjov@mts.ru>
2025-03-25 15:22:39 +0300

    add squareAndSort code

---
+++ src/main/kotlin/work/squareAndSort.kt
@@ -0,0 +1,21 @@
+package org.example.work
+
+fun main() {
+
+    val numbers = arrayOf(7, 4, 3, 8, 3)
+    val square = numbers.map { it * it }.toTypedArray()
+
+//        println(square.sorted())
+
+    for (i in square.indices) {
+        for (j in 0 until (square.size - 1 - i)) {
+            if (square[j] > square[j + 1]) {
+                val temp = square[j]
+                square[j] = square[j + 1]
+                square[j + 1] = temp
+
+            }
+        }
+    }
+    println(square.joinToString(prefix = "[", postfix = "]"))
+}
