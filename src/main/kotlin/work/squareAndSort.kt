a8247b919bf311607a91a8ae87f16af8e0a185ed
Ilya Ryzhov <isryjov@mts.ru>
2025-03-25 15:53:39 +0300

    обновил squareAndSort

--- src/main/kotlin/work/squareAndSort.kt
+++ src/main/kotlin/work/squareAndSort.kt
@@ -5,7 +5,8 @@ fun main() {
     val numbers = arrayOf(7, 4, 3, 8, 3)
     val square = numbers.map { it * it }.toTypedArray()
 
-//        println(square.sorted())
+    println(square.sorted()) // базовый метод
+    println(numbers.reversed()) // базовый метод
 
     for (i in square.indices) {
         for (j in 0 until (square.size - 1 - i)) {
