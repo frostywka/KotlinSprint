ce947fd2454cebdff0adb82fcba29f2f1f40c16d
Ilya Ryzhjov <frostywka@gmail.com>
2025-04-01 22:10:02 +0300

    KS-9-5 add homework 5 (Создание списка ингредиентов сложный вариант) урока ( List/список)

---
+++ src/main/kotlin/lesson_9_list/KS_9_5_createList_hard.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_9_list
+
+const val COUNT = 5
+
+fun main() {
+
+    println("Введите название 5 ингредиентов: ")
+    var ingredient: String
+    val list = mutableListOf<String>()
+
+    for (i in 0 until COUNT) {
+        ingredient = readln()
+        list.add(ingredient)
+    }
+
+    val sortList = list.sorted().toMutableList()
+    sortList[0] = sortList[0].replaceFirstChar { it.uppercase() }
+
+    println(
+        """
+        ${sortList.distinct()}
+    """.trimIndent()
+    )
+
+}
