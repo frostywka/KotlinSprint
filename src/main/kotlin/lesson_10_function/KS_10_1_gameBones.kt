4f1f426ddb98ff2fdaff54e1109d469c1843da41
Ilya Ryzhjov <frostywka@gmail.com>
2025-04-02 20:21:22 +0300

    KS-10-1 add homework1 (игра в кости) урок Функции(fun)

---
+++ src/main/kotlin/lesson_10_function/KS_10_1_gameBones.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_10_function
+
+fun main() {
+    val human = throwOfDice()
+    val robot = throwOfDice()
+
+    val win = when {
+        human < robot -> "Победила машина"
+        human > robot -> "Победило человечество"
+        human == robot -> "Победила дружба"
+        else -> "4ый вариант"
+    }
+    println(
+        """
+        Результат броска игрока $human
+        Результат броска машины $robot
+        $win
+    """.trimIndent()
+    )
+}
+
+fun throwOfDice(): Int {
+    val count = 1..6
+    return count.random()
+}
+
+
