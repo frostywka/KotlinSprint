de2077148fff992347a6767c7bbd89201acaba16
Ilya Ryzhjov <frostywka@gmail.com>
2025-04-03 19:38:23 +0300

    KS-11-theory

---
+++ src/main/kotlin/lesson_11_classes_propetries_objects/Dish.kt
@@ -0,0 +1,28 @@
+package org.example.lesson_11_classes_propetries_objects
+
+class Dish(
+    val id: Int,
+    val name: String,
+    var category: String,
+    val ingredients: List<String>,
+    var inFavorites: Boolean = false,
+) {
+
+    fun addToFavorites() {
+        println("Блюдо $name добавлено в избранное")
+        inFavorites = true
+    }
+
+    fun removeFromFavorites() {
+        println("Блюдо $name удалено из избранного")
+        inFavorites = false
+    }
+
+    fun startCooking() {
+        println("Пользователь перешел на экран начала приготовления блюда $name")
+    }
+
+    fun downloadIngredients() : List<String> {
+        return ingredients
+    }
+}
---
+++ src/main/kotlin/lesson_11_classes_propetries_objects/KS_11_theory.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_11_classes_propetries_objects
+
+fun main() {
+    //  выделить сущности
+    // опеределимть свойства
+    // наделить сущность действиями
+
+}
