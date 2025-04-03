ca044fe940e0fd4f9ca5951299916144741b4a8a
Ilya Ryzhjov <frostywka@gmail.com>
2025-04-03 21:28:17 +0300

    KS-8-4 урбрал лишний цикл - работает. Но не уверен, что это именно то о чем ты говорил

--- src/main/kotlin/lesson_8_array/KS_8_4_changeIngredientsArray.kt
+++ src/main/kotlin/lesson_8_array/KS_8_4_changeIngredientsArray.kt
@@ -1,17 +1,5 @@
 package org.example.lesson_8_array
 
-/*
-Представим, что мы получили массив ингредиентов.
-Программа должна распечатать весь список ингредиентов, затем заменить нужный ингредиент на новый по желанию пользователя.
- После этого программа должна показать обновленный список.
-
- – создай массив со списком ингредиентов и напечатай его в консоль;
- – запроси у пользователя информацию об ингредиенте, который он хочет заменить;
- – если пользователь ввел ингредиент, которого нет в списке, вывести соответствующее сообщение;
- – если есть, запроси ингредиент, который пользователь хотел бы добавить;
- – выведи обновленный список с сообщением: “Готово! Вы сохранили следующий список: [список_ингредиентов]”.
- */
-
 fun main() {
 
     println("Ингредиенты в текущем списке:")
@@ -25,17 +13,16 @@ fun main() {
     println("Какой ингредиент Вы хотите Добавить?")
     val newIngredient = readln()
 
-    for (i in listOfIngredients) {
-        if (i == oldIngredient) {
-            println("Ингредиент есть")
-            val indexIngredients = listOfIngredients.indexOf(oldIngredient)
-            listOfIngredients[indexIngredients] = newIngredient
+    if (oldIngredient in listOfIngredients) {
+        println("Ингредиент есть")
+        val indexIngredients = listOfIngredients.indexOf(oldIngredient)
+        listOfIngredients[indexIngredients] = newIngredient
 
-            for (j in listOfIngredients) {
-                println("Готово! Вы сохранили следующий список: [$j]")
-            }
-            return
+        for (j in listOfIngredients) {
+            println("Готово! Вы сохранили следующий список: [$j]")
         }
+        return
     }
     println("Ингредиента $oldIngredient в рецепте нет")
+
 }
