4d3b49ac3b2eb5447e31c507128eaec5d643f140
Ilya Ryzhjov <frostywka@gmail.com>
2025-04-03 21:13:45 +0300

    KS-11-2 update homework 2 (создание новых методов в классе) урок Классы.объекты

--- src/main/kotlin/lesson_11_classes_propetries_objects/KS_11_2_newMethodInClass/Main.kt
+++ src/main/kotlin/lesson_11_classes_propetries_objects/KS_11_2_newMethodInClass/Main.kt
@@ -2,4 +2,19 @@ package org.example.lesson_11_classes_propetries_objects.KS_11_2_newMethodInClas
 
 fun main() {
 
+    val user1 = User2(
+        id = 123,
+        login = "agent",
+        password = "007",
+        mail = "mail@mail.ru",
+    )
+
+    user1.printAllInfo()
+    println()
+    user1.changeBio()
+    println()
+    user1.changePassword()
+    println()
+    user1.printAllInfo()
+
 }
