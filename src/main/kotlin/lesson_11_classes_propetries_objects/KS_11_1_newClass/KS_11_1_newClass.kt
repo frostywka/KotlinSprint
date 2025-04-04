78cb09e21f6b2242490b995392a457d1a24b38c1
Ilya Ryzhjov <frostywka@gmail.com>
2025-04-04 18:34:12 +0300

    KS-11-1 update! вынес класс в файл с функцией

--- src/main/kotlin/lesson_11_classes_propetries_objects/KS_11_1_newClass/KS_11_1_newClass.kt
+++ src/main/kotlin/lesson_11_classes_propetries_objects/KS_11_1_newClass/KS_11_1_newClass.kt
@@ -1,5 +1,14 @@
 package lesson_11_classes_propetries_objects.KS_11_1_newClass
 
+data class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val mail: String,
+) {
+}
+
+
 fun main() {
 
     val user1 = User(
