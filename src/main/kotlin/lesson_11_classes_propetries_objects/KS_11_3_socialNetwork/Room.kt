1d01e3fc86f2865d83ff6a041d06b4791acb1267
Ilya Ryzhov <frostywka@gmail.com>
2025-04-07 17:27:48 +0300

    KS-11-3 add homework3 (socialNetwork - room) тема Классы

--- src/main/kotlin/lesson_11_classes_propetries_objects/KS_11_3_socialNetwork/Room.kt
+++ src/main/kotlin/lesson_11_classes_propetries_objects/KS_11_3_socialNetwork/Room.kt
@@ -1,54 +1,45 @@
 package org.example.lesson_11_classes_propetries_objects.KS_11_3_socialNetwork
-/*
-- обложка;
-- название;
-- список участников — отображаются в виде аватарок.
- При долгом нажатии на аватар подсветится его никнейм.
-  Рядом с аватаркой отображается текстовый бейдж с одним из возможных статусов говорящего:
-   “разговаривает”, “микрофон выключен”, “пользователь заглушен”.
-
-Класс должен содержать методы:
-– добавления участника (принимает объект участника и сохраняет в свойство комнаты);
-– обновления статуса (принимает имя пользователя и новый статус).
-
-После проектирования создай объект комнаты с произвольными данными.
- */
 
 data class Room(
     val coverRoom: String,
     val nameRoom: String,
     val listOfMembers: MutableList<String>,
-    val loginLongAvatarTap: String,
-    val muteStatus: Boolean,
-    val microOffStatus: Boolean,
-    val talkingStatus: Boolean,
+    val nickName: MutableList<String>,
+    var muteStatus: Boolean,
+    var microOffStatus: Boolean,
+    var talkingStatus: Boolean,
 
     ) {
 
     fun addMember() {
-        println("Введите имя кого добавить в комнату:")
+        println("Введите NickName кого добавить в комнату :")
         listOfMembers.add(readln())
     }
 
-
+    fun isTalkingStatus(){
+        println("Введите NickName пользователя, которому необходимо поменять статус")
+        nickName.add(readln())
+        println("Пользователь Включил микрофон")
+        muteStatus = true
+    }
 }
 
 fun main() {
 
     val room1 = Room(
-    coverRoom = "чтотоНарисовано",
-    nameRoom = "Котики",
-    listOfMembers = mutableListOf("Вася", "Петя", "Витя"),
-    loginLongAvatarTap = "HelloWorld",
-    muteStatus = true,
-    microOffStatus = true,
-    talkingStatus = true,
+        coverRoom = "чтотоНарисовано",
+        nameRoom = "Котики",
+        listOfMembers = mutableListOf("Pokemon", "Minion", "Santa"),
+        nickName = mutableListOf("Alex", "Mike", "Rose"),
+        muteStatus = true,
+        microOffStatus = true,
+        talkingStatus = true,
     )
 
-//    println(room1)
     println(room1.listOfMembers)
     room1.addMember()
     println(room1.listOfMembers)
     println()
-
+    room1.isTalkingStatus()
+    println(room1.talkingStatus)
 }
