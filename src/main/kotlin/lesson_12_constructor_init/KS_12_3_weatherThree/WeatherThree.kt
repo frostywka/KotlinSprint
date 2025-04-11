8d59340a3d88785ada3ce560588ae8cba93c8bfd
Ilya Ryzhov <frostywka@gmail.com>
2025-04-11 11:25:54 +0300

    Lesson-12-3-weatherThree , update ДЗ, урок Конструкторы

--- src/main/kotlin/lesson_12_constructor_init/KS_12_3_weatherThree/WeatherThree.kt
+++ src/main/kotlin/lesson_12_constructor_init/KS_12_3_weatherThree/WeatherThree.kt
@@ -3,12 +3,12 @@ package org.example.lesson_12_constructor_init.KS_12_3_weatherThree
 const val ONE_CELSIUS_KELVINS = -272
 
 class WeatherThree(_daysTempKelvins: Int, _nightsTempKelvins: Int, _weatherPrecipitationKelvins: Boolean) {
-    var daysTemp = _daysTempKelvins+ONE_CELSIUS_KELVINS
-    var nightsTemp = _nightsTempKelvins+ONE_CELSIUS_KELVINS
+    var daysTemp = _daysTempKelvins + ONE_CELSIUS_KELVINS
+    var nightsTemp = _nightsTempKelvins + ONE_CELSIUS_KELVINS
     var weatherPrecipitation = _weatherPrecipitationKelvins
 
     fun info() {
-        println("Температура днем $daysTemp в Цельсиях,Температура ночью $nightsTemp в Цельсиях, наличие остадков $weatherPrecipitation")
+        println("Температура днем $daysTemp в Цельсиях, Температура ночью $nightsTemp в Цельсиях, наличие остадков $weatherPrecipitation")
     }
 }
 
