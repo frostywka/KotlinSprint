9e6a35210011caaa23ecf560658e6eefe15c41e4
Ilya Ryzhov <frostywka@gmail.com>
2025-04-11 11:41:49 +0300

    Lesson-12-2-weatherTwo , update ДЗ, урок Конструкторы

--- src/main/kotlin/lesson_12_constructor_init/KS_12_2_weatherTwo/WeatherTwo.kt
+++ src/main/kotlin/lesson_12_constructor_init/KS_12_2_weatherTwo/WeatherTwo.kt
@@ -1,18 +1,15 @@
 package org.example.lesson_12_constructor_init.KS_12_2_weatherTwo
 
-class WeatherTwo(_dayTemp: Int, _nightTemp: Int, _osadki: Boolean) {
-    var dayTemp = _dayTemp
-    var nightTemp = _nightTemp
-    var osadki = _osadki
+data class WeatherTwo(val dayTemp: Int, val nightTemp: Int, val osadki: Boolean) {
 
     fun info() {
-        println("Температура днем $dayTemp,Температура ночью $nightTemp, наличие остадков $osadki")
+        println("Температура днем $dayTemp, Температура ночью $nightTemp, наличие остадков $osadki")
     }
 }
 
 fun main() {
-    val weather1 = WeatherTwo(15,4,true)
-    val weather2 = WeatherTwo(20,8,false)
+    val weather1 = WeatherTwo(15, 4, true)
+    val weather2 = WeatherTwo(20, 8, false)
 
     weather1.info()
     weather2.info()
