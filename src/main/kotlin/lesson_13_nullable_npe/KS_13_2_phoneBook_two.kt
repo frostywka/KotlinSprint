aba01fbe73286cd3ba5831f3a008380b4795bc5e
Ilya Ryzhov <ilya@rizhoff.ru>
2025-04-27 15:26:38 +0300

    KS-13-2-phoneBookTwo, дз 2 , урок nullable type

--- src/main/kotlin/lesson_13_nullable_npe/KS_13_2_phoneBook_two.kt
+++ src/main/kotlin/lesson_13_nullable_npe/KS_13_2_phoneBook_two.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_13_nullable_npe
 
-class PhoneBookTwo(val name: String, val phoneNumber: Long, val companyName: String?) {
+class ContactForm(val name: String, val phoneNumber: Long, val companyName: String?) {
 
     fun print() {
         println("Имя: $name, Номер: $phoneNumber, Компания: ${companyName ?: "<Строка не указана>"}")
@@ -9,7 +9,7 @@ class PhoneBookTwo(val name: String, val phoneNumber: Long, val companyName: Str
 
 fun main() {
 
-    val phoneBookTwo = PhoneBookTwo("Ilya", 79111111111, null)
+    val contact = ContactForm("Ilya", 79111111111, null)
 
-    phoneBookTwo.print()
+    contact.print()
 }
