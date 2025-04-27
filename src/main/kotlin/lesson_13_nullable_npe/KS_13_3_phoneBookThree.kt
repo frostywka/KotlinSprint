ee59041c9c134c45a90924d3f4ca687d0e5a5b12
Ilya Ryzhov <ilya@rizhoff.ru>
2025-04-27 15:54:56 +0300

    KS-13-3-phoneBookThree, дз 3 , урок nullable type

--- src/main/kotlin/lesson_13_nullable_npe/KS_13_3_phoneBookThree.kt
+++ src/main/kotlin/lesson_13_nullable_npe/KS_13_3_phoneBookThree.kt
@@ -1,23 +1,23 @@
 package org.example.lesson_13_nullable_npe
 
-class PhoneBookThree(val name: String, val phoneNumber: Long, val companyName: String?) {
+class NewContact(val name: String, val phoneNumber: Long, val companyName: String?)
 
-    fun printPhoneBook() {
-        println("Имя: $name, Номер: $phoneNumber, Компания: ${companyName ?: "<Строка не указана>"}")
-    }
-}
 
 fun main() {
 
     val phoneBookList = listOf(
-        PhoneBookThree("Вася", 11111111, null),
-        PhoneBookThree("Иван", 22222222, null),
-        PhoneBookThree("Леха", 33333333, "null"),
-        PhoneBookThree("Макс", 44444444, "Эхо"),
-        PhoneBookThree("Петя", 55555555, "Редит"),
+        NewContact("Вася", 11111111, null),
+        NewContact("Иван", 22222222, null),
+        NewContact("Леха", 33333333, "null"),
+        NewContact("Макс", 44444444, "Эхо"),
+        NewContact("Петя", 55555555, "Редит"),
     )
 
-    phoneBookList.forEach {
-        println("Название компании: ${it.companyName ?: "<строка не указана>"}")
-    }
+   val printCompany = phoneBookList.filter {
+       it.companyName != null
+   }
+
+   printCompany.forEach {
+       println("Название компании: ${it.companyName}")
+   }
 }
