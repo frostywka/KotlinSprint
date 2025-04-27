8c3a83f60480d3e3f02554eaad6b04a7106db01f
Ilya Ryzhov <ilya@rizhoff.ru>
2025-04-27 15:29:38 +0300

    KS-13-2-phoneBookTwo, дз 2 , урок nullable type

--- src/main/kotlin/lesson_13_nullable_npe/KS_13_2_phoneBook_two.kt
+++ src/main/kotlin/lesson_13_nullable_npe/KS_13_2_phoneBook_two.kt
@@ -3,7 +3,7 @@ package org.example.lesson_13_nullable_npe
 class ContactForm(val name: String, val phoneNumber: Long, val companyName: String?) {
 
     fun print() {
-        println("Имя: $name, Номер: $phoneNumber, Компания: ${companyName ?: "<Строка не указана>"}")
+        println("Имя: $name,\nНомер: $phoneNumber,\nКомпания: ${companyName ?: "<Строка не указана>"}")
     }
 }
 
