63eee21e73d08253fe8c6201e4a7cd7b687f3a4c
Ilya Ryzhov <ilya@rizhoff.ru>
2025-04-29 17:48:22 +0300

    KS-14-1-gameTwo // ДЗ-1 // урок наследование Изменил классы, убрал лишнее

--- src/main/kotlin/lesson_14_override_inheritance_openclass/theory/KS_14_1/KS_14_1_gameOne.kt
+++ src/main/kotlin/lesson_14_override_inheritance_openclass/theory/KS_14_1/KS_14_1_gameOne.kt
@@ -1,16 +1,30 @@
 package org.example.lesson_14_override_inheritance_openclass.theory.KS_14_1
 
+open class Liner(
+    val name: String = "Лайнер",
+    val numberOfPassengers: Int = 1000,
+    val speed: Int = 5000,
+    val loadCapacity: Int = 100,
+    val breakingIce: Boolean = false,
+) {
+}
+
+class Cargo(
+    name: String,
+): Liner(name, numberOfPassengers = 100, speed = 500, loadCapacity = 1000) {
+}
+
+class Icebreaker(
+    name: String,
+    numberOfPassengers: Int = 50,
+    speed: Int = 100,
+    loadCapacity: Int = 50,
+): Liner(name, numberOfPassengers, speed, loadCapacity, breakingIce = true) {
+}
+
 fun main() {
 
     val liner1 = Liner()
-    liner1.morePassengers()
-    liner1.allInfo()
-    println()
-
-    val cargo1 = Cargo("Грузовой", 100,500,1000)
-    cargo1.allInfo()
-    println()
-
-    val icebreaker = Icebreaker("Ледокол", 50, 100,50)
-    icebreaker.allInfo()
+    val cargo1 = Cargo("CargoOne")
+    val icebreaker1 = Icebreaker("Ледокол")
 }
