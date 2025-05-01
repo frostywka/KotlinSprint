ba51c1b446f328e066c8d06900d624ff3f493d85
ilyaRZ <frostywka@gmail.com>
2025-05-01 22:41:34 +0300

    KS-13-4 // ДЗ-4 // урок nullable правки

--- src/main/kotlin/lesson_13_nullable_npe/KS_13_4_phoneBookFour.kt
+++ src/main/kotlin/lesson_13_nullable_npe/KS_13_4_phoneBookFour.kt
@@ -1,9 +1,9 @@
 package org.example.lesson_13_nullable_npe
 
-class ContactForm(val name: String, val phoneNumber: Long?, val companyName: String?) {
+class ContactForm(val name: String, val phoneNumber: Long, val companyName: String?) {
 
     fun printPhoneBook() {
-        println("Имя: $name, Номер: ${phoneNumber ?: "<Строка не указана>"}, Компания: ${companyName ?: "<Строка не указана>"}")
+        println("Имя: $name, Номер: $phoneNumber, Компания: ${companyName ?: "<Строка не указана>"}")
     }
 }
 
@@ -14,11 +14,22 @@ fun main() {
     while (true) {
         println("Введите Имя контакта:")
         val name = readln()
+        if (name.isEmpty()) {
+            println("Имя контакта не введено")
+            break
+        }
 
         println("Введите Номер контакта:")
         val phoneNumberInput = readln()
 
-        if (phoneNumberInput.isBlank()) {
+//        val phoneNumber: Long
+
+//        when {
+//            phoneNumberInput.isBlank() -> {
+//                println("Номер телефона не введен. Контакт не будет добавлен")
+//                continue
+//            }
+        if (phoneNumberInput.isEmpty()) {
             println("Номер телефона не введен. Контакт не будет добавлен")
             continue
         }
@@ -28,16 +39,33 @@ fun main() {
             println("Номер телефона должен быть числом. Контакт не будет добавлен")
             continue
         }
+//
+//            phoneNumberInput.toLongOrNull() == null -> {
+//                println("Номер телефона должен быть числом. Контакт не будет добавлен")
+//                continue
+//            }
+//
+//            else -> {
+//                phoneNumber = phoneNumberInput.toLong()
+//                println("Номер телефона успешно добавлен")
+//            }
+
 
         println("Введите Компанию контакта:")
-        val companyName = readln().takeIf { it.isNotBlank() }
+//        val companyName = readln().trim().takeIf { it.isNotEmpty() }
+        val companyName = readln().ifBlank {
+            null
+        }
 
-        phoneBookList.add(ContactForm(name, phoneNumber, companyName.toString()))
 
-
+        phoneBookList.add(ContactForm(name, phoneNumber, companyName))
+        println("Контакт добавлен")
         phoneBookList.forEach {
             it.printPhoneBook()
         }
-
+        break
     }
 }
+
+
+
