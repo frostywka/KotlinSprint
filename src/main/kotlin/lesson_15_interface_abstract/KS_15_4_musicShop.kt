73c96b7e66c415be1b8c1c74fff15cd70fa536a8
Ilya Ryzhov <ilya@rizhoff.ru>
2025-05-12 15:43:40 +0300

    KS-15-4-mobileGame // ДЗ 4 // abstract+interface update

--- src/main/kotlin/lesson_15_interface_abstract/KS_15_4_musicShop.kt
+++ src/main/kotlin/lesson_15_interface_abstract/KS_15_4_musicShop.kt
@@ -2,24 +2,29 @@ package org.example.lesson_15_interface_abstract
 
 interface Search {
     fun startSearch() {
-        println("Выполняется поиск")
+//        println("Выполняется поиск") // можно так, а можно с реализацией через класс(лучше через класс)
+//    }
     }
-}
 
-open class Products(val name: String, val quantity: Int)
+    open class Products(val name: String, val quantity: Int)
 
-class Instrument(
-    name: String,
-    quantity: Int
-) : Search, Products(name, quantity)
+    class Instrument(
+        name: String,
+        quantity: Int
+    ) : Search, Products(name, quantity) {
+        override fun startSearch() {
+            println("Выполняется поиск")
+        }
+    }
 
-class Accessories(
-    name: String,
-    quantity: Int
-) : Products(name, quantity)
+    class Accessories(
+        name: String,
+        quantity: Int
+    ) : Products(name, quantity)
 
-fun main() {
+    fun main() {
 
-    val instrument = Instrument("Guitar", 9)
-    instrument.startSearch()
+        val instrument = Instrument("Guitar", 9)
+        instrument.startSearch()
+    }
 }
