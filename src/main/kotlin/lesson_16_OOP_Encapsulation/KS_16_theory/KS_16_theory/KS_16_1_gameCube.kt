27ec67fc2e6d92d198d176da4fddcb3bdae77b58
Ilya Ryzhov <ilya@rizhoff.ru>
2025-05-13 14:17:26 +0300

    KS-16-1-gameCube // ДЗ-1 // тема инкапсуляция // Update

--- src/main/kotlin/lesson_16_OOP_Encapsulation/KS_16_theory/KS_16_theory/KS_16_1_gameCube.kt
+++ src/main/kotlin/lesson_16_OOP_Encapsulation/KS_16_theory/KS_16_theory/KS_16_1_gameCube.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_16_OOP_Encapsulation.KS_16_theory.KS_16_theory
 
 class Cube() {
-    private val number: IntRange = 1..6
+    private val number: Int = (1..6).random()
 
     fun printRandomNumber() {
-        println(number.random())
+        println(number)
     }
 }
 
