3e7f1f2459f7a2a8cdaf2722b3a10ba3ee73f124
Ilya Ryzhov <ilya@rizhoff.ru>
2025-05-13 14:12:40 +0300

    KS-16-2-circle // ДЗ-2 // тема инкапсуляция

---
+++ src/main/kotlin/lesson_16_OOP_Encapsulation/KS_16_theory/KS_16_theory/KS_16_2_circle.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_16_OOP_Encapsulation.KS_16_theory.KS_16_theory
+
+private const val PI = 3.14
+
+class Circle(private val radius: Int = 7) {
+
+    fun areaCircle() {
+        val area = PI * radius * radius
+        println("Площадь круга равна: $area")
+    }
+
+    fun perimeterCircle() {
+        val perimeter = 2 * PI * radius
+        println("Периметр круга равен $perimeter")
+
+    }
+}
+
+fun main() {
+
+    val circle = Circle()
+    circle.areaCircle()
+    circle.perimeterCircle()
+
+
+}
