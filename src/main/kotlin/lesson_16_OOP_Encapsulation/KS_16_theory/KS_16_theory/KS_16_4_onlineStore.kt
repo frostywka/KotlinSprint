6a6b93ca67bc7ab71c7316b1c93abee44c6887e7
Ilya Ryzhov <ilya@rizhoff.ru>
2025-05-18 16:24:28 +0300

    KS-16-4-onlineStore // ДЗ-4 // тема Инкапсуляция //

--- src/main/kotlin/lesson_16_OOP_Encapsulation/KS_16_theory/KS_16_theory/KS_16_4_onlineStore.kt
+++ src/main/kotlin/lesson_16_OOP_Encapsulation/KS_16_theory/KS_16_theory/KS_16_4_onlineStore.kt
@@ -1,19 +1,21 @@
 package org.example.lesson_16_OOP_Encapsulation.KS_16_theory.KS_16_theory
 
-class Order(private val orderNumber: Int, val readyStatus: Boolean){
+class Order(private val orderNumber: Int, var readyStatus: Boolean) {
 
-    fun createRequest(){
-
+    fun createRequestToChangeStatus() { // имитация новой заявки
+        changeStatusOrder()
     }
 
-    fun changeStatusOrder(){
-
+    fun changeStatusOrder() { //
+        readyStatus = true
     }
 
 }
 
 fun main() {
 
-    val order = Order(1111,false)
+    val order = Order(1111, false)
+    order.createRequestToChangeStatus()
 
+    println(order.readyStatus)
 }
