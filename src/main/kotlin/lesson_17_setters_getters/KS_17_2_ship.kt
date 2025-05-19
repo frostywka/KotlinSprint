a76380def5a4b594bea854817c57d42520ec23bc
Ilya Ryzhov <ilya@rizhoff.ru>
2025-05-19 16:46:06 +0300

    KS-17-2-ship // ДЗ-2 // тема getters+setter

---
+++ src/main/kotlin/lesson_17_setters_getters/KS_17_2_ship.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_17_setters_getters
+
+class Ship(
+    val nameParam: String,
+    val middleSpeedParam: Double,
+    val portParam: String,
+){
+    var name = nameParam
+        get() = field
+        set(value) {
+            field = "Поле с Именем - Неизменно"
+        }
+}
+
+fun main() {
+
+    val ship = Ship("FireFlight",4000.24,"BlackStar")
+    println("Оригинальное название корабля ${ship.name}")
+    ship.name = "Sokol"
+    println("При попытке изменить имя имя корабля стало: ${ship.name}")
+
+}
