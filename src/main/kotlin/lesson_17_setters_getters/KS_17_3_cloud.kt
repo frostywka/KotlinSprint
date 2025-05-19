83e1cd60fda634ac75facd4943e64f240ac4dadf
Ilya Ryzhov <ilya@rizhoff.ru>
2025-05-19 19:26:47 +0300

    KS-17-3-cloud // ДЗ-3 // тема getters+setter // update

--- src/main/kotlin/lesson_17_setters_getters/KS_17_3_cloud.kt
+++ src/main/kotlin/lesson_17_setters_getters/KS_17_3_cloud.kt
@@ -1,24 +1,23 @@
 package org.example.lesson_17_setters_getters
 
-class Folder(val nameParam: String, val countFilesParam: Int, val flagParam: String) {
+class Folder(val nameParam: String, val countFilesParam: Int, val isSecret: Boolean) {
 
     val name = nameParam
-        get() = when {
-            flagParam == "секретно" -> "Скрытая папка, количество файлов в ней: 0"
-            flagParam != "секретно" -> "Название папки: $field, количество файлов: $countFilesParam"
-            else -> field
-        }
+        get() = if (isSecret)
+            "Скрытая папка, количество файлов в ней: 0"
+        else
+            "Название папки: $field, количество файлов: $countFilesParam"
 }
 
 fun main() {
 
-    val folder = Folder("Games", 3, "секретно")
+    val folder = Folder("Games", 3, true)
     println(folder.name)
 
-    val folder1 = Folder("PornHub", 1000, "опен")
+    val folder1 = Folder("PornHub", 1000, false)
     println(folder1.name)
 
-    val folder2 = Folder("Files", 10, "хурма")
+    val folder2 = Folder("Files", 10, false)
     println(folder2.name)
 
 }
